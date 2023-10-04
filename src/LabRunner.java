9e19d135d4620e7cc1645c5933f97ddd0d9385c8
BT_2N2_19 <sample@gmail.com>
2023-10-04 13:57:46 -0400

    Code done

--- src/LabRunner.java
+++ src/LabRunner.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class LabRunner {
+public class LabRunner {
+    public static void main(String args[]) {
+        System.out.println("Cows say \" moo \"!");
+        System.out.println("Brooklyn Tech\n29 Ft. Greene\nBrooklyn");
+        System.out.println("The string \"\"\nis an empty string.");
+        System.out.println("The escape sequence for \\ is \\\\");
+        System.out.println("The escape sequence for \" is \\\"");
+        System.out.println("My favorite punctuation mark is \"\\\"");
+        System.out.println("A\n B\n  C’s");
+        System.out.println("Dear Jen,\nYou got an \"A\"!\n\nSincerely,\nThe teacher");
+        System.out.println("/A\\V/A\\\n\\V/A\\V/");
+        System.out.println("  /\\\n /\"\"\\\n/\"  \"\\\n\\\"  \"/\n \\\"\"/\n  \\/");
+    }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,64 @@
-package PACKAGE_NAME;public class Student {
-}
+    public class Student {
+
+        private String firstName;
+        private String lastName;
+        private int gradYear;
+        private double accumulatedTestScores;
+        private int testScoreCount;
+
+        public Student(String firstName, String lastName, int gradYear) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.gradYear = gradYear;
+            accumulatedTestScores = 0.0;
+            testScoreCount = 0;
+        }
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public void setGradYear(int newGradYear) {
+            gradYear = newGradYear;
+        }
+
+        public void addTestScore(double newTestScore) {
+            accumulatedTestScores += newTestScore;
+            testScoreCount++;
+        }
+
+        public boolean isPassing() {
+            if (averageTestScore() >= 65) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        public double averageTestScore() {
+            double average = accumulatedTestScores / testScoreCount;
+            return average;
+        }
+
+        public void printStudentInfo() {
+            System.out.println("Student Full Name: " + firstName + " " + lastName);
+            System.out.println("Graduation Year: " + gradYear);
+            System.out.println("Number of tests: " + testScoreCount);
+            System.out.println("Average Test Score: " + averageTestScore());
+            System.out.println("Is passing: " + isPassing());
+        }
+
+        // add a new getStudentInfo() method here
+        public String getStudentInfo() {
+            String msg = "Student Full Name: " + firstName + " " + lastName + "\n";
+            msg += "Graduation Year: " + gradYear + "\n";
+            msg += "Number of tests: " + testScoreCount + "\n";
+            msg += "Average Test Score: " + averageTestScore() + "\n";
+            msg += "Is passing: " + isPassing();
+            return msg;
+        }
+    }
